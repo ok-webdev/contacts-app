021b3e10eb4cde473e8db88ac67868d0fb4ff95f
Oleg K <oleg.kokoshinskii@gmail.com>
2023-01-09 16:41:14 +0600

    change button

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,7 +2,7 @@
  <header class="d-flex justify-space-between align-center">
   <h1>Contacts</h1>
 
-  <v-btn color="primary" @click="$emit('btnClick')" class="align-center"
+  <v-btn variant="tonal" color="primary" @click="$emit('btnClick')" class="align-center"
    ><v-icon aria-hidden="false"> mdi-plus </v-icon><span>Add</span></v-btn
   >
  </header>
