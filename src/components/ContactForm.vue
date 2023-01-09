1ee3c28a6329dcbf37eda1f5c616676da00431b8
Oleg K <oleg.kokoshinskii@gmail.com>
2023-01-09 16:44:05 +0600

    add contact-form styles/logic

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,66 +1,136 @@
 <template>
- <v-form class="form bg-white rounded-lg pa-3">
-  <h2>Add contact</h2>
+ <v-form
+  @submit.prevent="submitHandler"
+  @reset="cancelHandler"
+  class="form bg-white rounded-lg pa-6"
+ >
+  <div class="mb-3">
+   <h2>{{ props.mode === 'add' ? 'Add' : 'Edit' }} contact</h2>
+  </div>
   <v-text-field
-   v-model="formData.name"
-   :rules="nameRules"
+   v-model="formData.firstName"
+   :rules="rules"
    label="First name"
+   color="primary"
+   variant="underlined"
    required
+   hint="required"
+   persistent-hint
   />
   <v-text-field
-   v-model="formData.name"
-   :rules="nameRules"
+   v-model="formData.lastName"
+   :rules="rules"
    label="Last name"
+   color="primary"
+   variant="underlined"
    required
+   hint="required"
+   persistent-hint
   />
   <v-text-field
-   v-model="formData.name"
-   :rules="nameRules"
+   v-model="formData.email"
+   :rules="rules"
    label="Email"
+   color="primary"
+   variant="underlined"
    required
+   hint="required"
+   persistent-hint
   />
   <v-text-field
-   v-model="formData.name"
-   :rules="nameRules"
-   label="Company"
-  />
-  <v-text-field
-   v-model="formData.name"
-   :rules="nameRules"
-   label="Job title"
-  />
-  <v-text-field
-   v-model="formData.name"
-   :rules="nameRules"
+   v-model="formData.phone"
+   :rules="rules"
    label="Phone"
+   color="primary"
+   variant="underlined"
+   hint="required"
+   persistent-hint
   />
-  <div class="d-flex justify-center">
-    <v-btn color="error">Cancel</v-btn>
-    <v-btn color="primary" class="ml-3">Submit</v-btn>
+  <v-text-field
+   v-model="formData.company"
+   label="Company"
+   color="primary"
+   variant="underlined"
+   hint="Fill this field for company (not required)"
+   persistent-hint
+  />
+  <v-text-field
+   v-model="formData.jobTitle"
+   label="Job title"
+   color="primary"
+   variant="underlined"
+   hint="Fill this field for job title (not required)"
+   persistent-hint
+  />
+  <v-select
+   label="Select"
+   v-model="formData.country"
+   :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
+   color="primary"
+   variant="underlined"
+   hint="Select country (not required)"
+   persistent-hint
+  />
+
+  <div class="d-flex justify-center mt-3">
+   <v-btn type="reset" variant="tonal" color="error">Cancel</v-btn>
+   <v-btn type="submit" variant="tonal" color="primary" class="ml-3"
+    >Submit</v-btn
+   >
   </div>
  </v-form>
 </template>
 
 <script setup>
  import { ref } from 'vue';
+ const props = defineProps({
+  mode: {
+   type: String,
+   default: 'add',
+  },
+ });
+ const emit = defineEmits(['submit', 'cancel']);
  const formData = ref({
   firstName: '',
-  lastNmae: '',
+  lastName: '',
   email: '',
-  company: '',
-  jobTille: '',
-  phone: ''
+  country: undefined,
+  company: undefined,
+  jobTitle: undefined,
+  phone: '',
  });
 
- const nameRules = [
-  (v) => !!v || 'Name is required'
- ];
+ const rules = [(v) => !!v || 'Field is required'];
+
+ function submitHandler() {
+  if (
+   formData.value.firstName.length > 0 &&
+   formData.value.lastName.length > 0 &&
+   formData.value.email.length > 0 &&
+   formData.value.phone.length > 0
+  ) {
+   emit('submit', { ...formData.value });
+  }
+ }
+
+ function cancelHandler() {
+  formData.value = {
+   firstName: '',
+   lastName: '',
+   email: '',
+   country: undefined,
+   company: undefined,
+   jobTitle: undefined,
+   phone: '',
+  };
+  emit('cancel');
+ }
 </script>
 
 <style scoped>
-  .form {
-    min-width: 300px;
-    width: 450px;
-    background-color: aliceblue;
-  }
+ .form {
+  min-width: 200px;
+  width: 350px;
+  background-color: aliceblue;
+ }
 </style>
